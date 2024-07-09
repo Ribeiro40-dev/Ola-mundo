0f688aa670ed03412ce6bb66449228611060f072
Ribeiro40-dev <cunhamaione@gmail.com>
2024-07-09 02:02:08 -0300

    aplicativo

---
+++ aplicativo.py
@@ -0,0 +1,14 @@
+from tkinter import  *
+
+root=Tk()
+root.title('Projeto Saúde')
+
+img = PhotoImage(file = "images/imagem.png")
+
+label_image = Label(root, image=img).pack()
+
+
+
+
+
+root.mainloop()
